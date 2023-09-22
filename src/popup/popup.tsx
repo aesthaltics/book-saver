4e822d6fee2ef5da41884fac191c0bf651a52746
aesthaltics <aesthaltics@pm.me>
2023-09-22 21:09:08 +0200

    added tailwindcss

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import {createRoot} from 'react-dom/client'
+import "../styles/tailwind.css"
 type Props = {}
 const Popup = (props: Props) => {
   return (
-	<div>Popup</div>
+	<div className="h-64 w-64 bg-red-500">Popup</div>
   )
 }
 export default Popup

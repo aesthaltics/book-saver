8b60b2cd746500fb023c38adeb9008e92c8f4d9e
aesthaltics <aesthaltics@pm.me>
2023-09-23 18:18:23 +0200

    initial book and searchLink types

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,22 @@
 declare module "*.svg";
 declare module "*.css";
+
+declare type Book = {
+	title: string;
+	authors: string[];
+	cover: {
+		small?: string;
+		medium?: string;
+		large?: string;
+	};
+	id: any;
+	notes?: Map<string, string> | undefined;
+};
+
+declare type SearchLink = {
+	name: string;
+	url: string;
+	icon: string;
+	id: any;
+	isActive: boolean;
+};
